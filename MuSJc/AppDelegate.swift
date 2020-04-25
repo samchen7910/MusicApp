fe8b2876048122f67002b81078bd053a1fe6a1a6
Tran Gia Huy <ghuy.dtt@gmail.com>
2020-04-25 15:32:50 +0200

    Initial project setup

---
+++ MuSJc/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  MuSJc
+//
+//  Created by Tran Gia Huy on 4/25/20.
+//  Copyright © 2020 Tran Gia Huy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		window = UIWindow(frame: UIScreen.main.bounds)
+		window?.makeKeyAndVisible()
+		window?.rootViewController = ViewController()
+		return true
+	}
+
+
+}
+
---
+++ MuSJc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MuSJc
+//
+//  Created by Tran Gia Huy on 4/25/20.
+//  Copyright © 2020 Tran Gia Huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .white
+	}
+
+
+}
+
